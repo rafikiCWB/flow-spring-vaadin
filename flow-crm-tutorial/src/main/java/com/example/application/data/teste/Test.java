9e6bdd9cf83a07c1323052b578b2b003d49b4dbc
grando <sta_rafinha@hotmail.com>
2023-05-24 12:55:14 -0300

    Teste

---
+++ flow-crm-tutorial/src/main/java/com/example/application/data/teste/Test.java
@@ -0,0 +1,10 @@
+package com.example.application.data.teste;
+
+public class Test {
+
+    public static String nome = "hello";
+    public static void main(String[] args) {
+
+        System.out.println(nome);
+    }
+}
