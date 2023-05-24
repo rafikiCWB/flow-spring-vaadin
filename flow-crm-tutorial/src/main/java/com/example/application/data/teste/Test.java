127110d03e97339b010f47d82a22cf293b2193c2
grando <sta_rafinha@hotmail.com>
2023-05-24 12:56:48 -0300

    Hello_world

--- flow-crm-tutorial/src/main/java/com/example/application/data/teste/Test.java
+++ flow-crm-tutorial/src/main/java/com/example/application/data/teste/Test.java
@@ -2,7 +2,7 @@ package com.example.application.data.teste;
 
 public class Test {
 
-    public static String nome = "hello";
+    public static String nome = "hello_world";
     public static void main(String[] args) {
 
         System.out.println(nome);
