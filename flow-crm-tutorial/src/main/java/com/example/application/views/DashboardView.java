a7f0a31b52750b250991294006a35db8c1bf3bcf
Rafael Grando <sta_rafinha@hotmail.com>
2023-08-14 21:10:41 -0300

    Add user authentication and security checks
    Introduced a new security service, providing user authentication function. Added dependencies in pom.xml file for PostgreSql and Spring Security. Adjusted the structure of data in data.sql for better clarity. Altered the views to include security annotations. Created a login view for user authentication. Added a logout button in MainLayout, displaying the currently logged in user.

--- flow-crm-tutorial/src/main/java/com/example/application/views/DashboardView.java
+++ flow-crm-tutorial/src/main/java/com/example/application/views/DashboardView.java
@@ -11,7 +11,9 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.theme.lumo.LumoUtility;
+import jakarta.annotation.security.PermitAll;
 
+@PermitAll
 @Route(value = "dashboard", layout = MainLayout.class)
 @PageTitle("Dashboard | Vaadin CRM")
 public class DashboardView extends VerticalLayout {
